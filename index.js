43474eba781ed76b1e01167177a5b7356c68f000
Elizabeth <ekkjohnson@gmail.com>
2022-04-15 18:52:16 -0500

    engineer class

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const generateHTML = require('./src/generateHTML');
 
 // team profiles
-const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern'); 
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern'); 
 
 // node modules 
 const fs = require('fs'); 
