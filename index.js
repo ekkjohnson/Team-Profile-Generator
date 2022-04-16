bdf48feccfd7ec8d689dd3bf639d6c7ba2b54ebd
Elizabeth <ekkjohnson@gmail.com>
2022-04-15 19:22:44 -0500

    generate html

--- index.js
+++ index.js
@@ -12,8 +12,50 @@ const inquirer = require('inquirer');
 // team array
 const teamArray = []; 
 
-const newManager(manager) {
-    return inquirer.prompt([
+// Create function to initialize the app, divide it up based on employee roles
+// const newManager(manager) {
+//     return inquirer.prompt([
 
-    ])
+//     ])
+// }
+// helper function in your src folder to generate HTML file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateHTML(data), (err) => {
+        if (err) {
+            console.log('error');
+        } else {
+
+        }
+    })
 }
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            writeToFile('html.index', answers)
+        })
+}
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateHTML(data), (err) => {
+        if (err) {
+            console.log('error');
+        } else {
+
+        }
+    })
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            writeToFile('html.index', answers)
+        })
+}
+
+// Function call to initialize app
+init();
