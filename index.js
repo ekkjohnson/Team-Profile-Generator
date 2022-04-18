e68bcca7e73ddb9b2050f422b8805e248c12c894
Elizabeth <ekkjohnson@gmail.com>
2022-04-18 18:57:19 -0500

    write file

--- index.js
+++ index.js
@@ -134,3 +134,28 @@ function newMember (){
 
 // Function call to initialize app
 init();
+
+const writeFile = data => {
+    fs.writeFile('./dist/index.html', data, err => {
+        // if there is an error 
+        if (err) {
+            console.log(err);
+            return;
+        // when the profile has been created 
+        } else {
+            console.log("Your team profile has been successfully created! Please check out the index.html")
+        }
+    })
+}; 
+
+addManager()
+  .then(addEmployee)
+  .then(teamArray => {
+    return generateHTML(teamArray);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .catch(err => {
+ console.log(err);
+  });
